e8a6e1e6d57c12e7275db4d6a937580c604b829a
Rodrigo Castilho <rcastilho@gmail.com>
2022-11-06 20:41:41 -0300

    variavelAmbiente

--- src/data/db/db.js
+++ src/data/db/db.js
@@ -1,14 +1,12 @@
 const mysql = require('mysql2');
 const pool = mysql.createPool({
     connectionLimit: 5,
-    host: 'containers-us-west-82.railway.app',
-    user: 'root',
-    password: 'rorKaofVhR3NVviPJReI',
-    database: 'railway',
-    port: '7439'
-
+    host: process.env.HOST,// 'containers-us-west-82.railway.app',
+    user: process.env.USER, // 'root',
+    password: process.env.PASSWORD, //'rorKaofVhR3NVviPJReI',
+    database: process.env.DATABASE,//'railway',
+    port: process.env.PORT //'7439'
 })
-
 
 module.exports = pool
 
