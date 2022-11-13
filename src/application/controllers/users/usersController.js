d726c1768bae7313da3bcbc75e127008cf9f03a5
Rodrigo Castilho <rcastilho@gmail.com>
2022-11-13 00:36:58 -0300

    auth

--- src/application/controllers/users/usersController.js
+++ src/application/controllers/users/usersController.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const _userService = require('../../../service/userService/userService')
 const validator = require('../../../crosscutting/validator/validator')
 const { userValidator, userUpdate } = require('../../../crosscutting/validator/userValidator')
+const authUser = require('../../../crosscutting/auth/authUsers')
 
-router.get('/', _userService.getAllUsers)
+router.get('/', authUser, _userService.getAllUsers)
 router.post('/', userValidator(), validator, _userService.postAddUser)
-router.patch('/:id', userUpdate(), validator, _userService.updateUser)
-router.delete('/:id', _userService.deleteUser)
+router.patch('/:id', userUpdate(), validator, authUser, _userService.updateUser)
+router.delete('/:id', authUser, _userService.deleteUser)
 
 module.exports = router
--- src/crosscutting/auth/authUsers.js
+++ src/crosscutting/auth/authUsers.js
@@ -5,27 +5,26 @@ const sql = require('../../data/db/db')
 const authUser = async (req, res, next) => {
     try {
         const carregaToken = req.headers.authorization
-        const token = carregaToken.split(' ')[1]
-        if (!carregaToken) {
-            res.json({ 'message': 'não autorizado' })
+        const token = carregaToken.split(' ')[1] || ''
+        if (!token) {
+            res.status(400).json({ 'message': 'não autorizado' })
             return
         } else {
             const validaToken = jwt.verify(token, process.env.JWT_SECRET)
-            console.log(validaToken.data)
             const result = await _loginRepository.getUserByEmail(validaToken.data[0].email)
             sql.query(result.query, result.fields, (err, data) => {
                 err && res.json({ err })
                 if (!data.length) {
                     res.status(400).json({ 'message': 'erro ao autenticar' })
+                    return
                 } else {
                     next()
                 }
             })
         }
     } catch (error) {
-        res.status(400).json({ 'message': 'token inválido'})
+        res.status(400).json({ 'message': 'token inválido' })
         return error
-
     }
 }
 
