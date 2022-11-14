f41b996c0e586e086f538a55c96a0c06901e02c2
Rodrigo Castilho <rcastilho@gmail.com>
2022-11-13 23:22:49 -0300

    detalhes

--- src/application/routes/router.js
+++ src/application/routes/router.js
@@ -5,14 +5,19 @@ router.use('/api/users', require('../controllers/users/usersController'))
 router.use('/api/login', require('../controllers/login/loginController'))
 
 //pagina home
-router.get('/api',  (req, res) => {
+router.get('/api', (req, res) => {
     try {
         const links = {
             "home": "https://user-auth-api.up.railway.app/api",
-            "login": "https://user-auth-api.up.railway.app/api/login (método POST)",
-            "register": "https://user-auth-api.up.railway.app/api/users (método POST)",
-            "auth": "https://user-auth-api.up.railway.app/api/login/auth (método Get, com acesso após login e envio de token válido)",
-            "getAll": "https://user-auth-api.up.railway.app/api/users?itensPerPage=XXX&page=XXX (Definir numero de itens e pagina para paginação)"
+            "details": "",
+            "login": "https://user-auth-api.up.railway.app/api/login",
+            "details": "(método POST)",
+            "register": "https://user-auth-api.up.railway.app/api/users",
+            "details": "(método POST)",
+            "auth": "https://user-auth-api.up.railway.app/api/login/auth",
+            "details": " (método Get, com acesso após login e envio de token válido)",
+            "getAll": "https://user-auth-api.up.railway.app/api/users?itensPerPage=5&page=0",
+            "details": "(Definir numero de itens e pagina para paginação)"
         }
         const autor = {
             "desenvolvidoPor": "rcastilho",
