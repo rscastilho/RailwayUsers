bb0067535e2fde7a269c605e357bf3981f0fcc20
Rodrigo Castilho <rcastilho@gmail.com>
2022-11-19 19:14:40 -0300

    personalCors

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-// app.use(cors({
-//     credentials: true,
+// app.use(cors())
+app.use(cors({
+    credentials: true,
     
-//     origin: 'http://localhost:3000'
-// }))
+    origin: ['http://localhost:3000', 'https://usersauth-react.vercel.app/']
+}))
 
 const router = require('./src/application/routes/router')
 app.use(router)
