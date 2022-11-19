65e27008260ad28888dfc3c8f7bedacd083792c2
Rodrigo Castilho <rcastilho@gmail.com>
2022-11-19 19:11:55 -0300

    corstoall

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    credentials: true,
+app.use(cors())
+// app.use(cors({
+//     credentials: true,
     
-    origin: 'http://localhost:3000'
-}))
+//     origin: 'http://localhost:3000'
+// }))
 
 const router = require('./src/application/routes/router')
 app.use(router)
